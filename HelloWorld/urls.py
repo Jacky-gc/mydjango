0d004bc8ac2edb0a7c2932f638fd35817cb4076a
Jacky-gc <Georg358942671>
2018-10-17 19:10:38 +0800

    Class Experience

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+#urlpatterns = [
+ #   path('admin/', admin.site.urls),
+#]
+ 
+ 
+from newworld import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+            url(r'index/', views.index, name='index'),
 ]
